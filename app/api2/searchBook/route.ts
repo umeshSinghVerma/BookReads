dceb02b44e4fe25ba346bd66797c16a2c6b307b2
umeshSinghVerma <usv0308@gmail.com>
2024-01-04 05:03:39 +0530

    bug fixes

--- app/api2/searchBook/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import bookSearch from "../utilFunctions/bookSearch";
-
-export async function GET(req: NextRequest) {
-    const searchParams = req.nextUrl.searchParams
-    const bookserachName = JSON.stringify(searchParams.get('book'))
-    const bookName = await bookSearch(bookserachName);
-    return NextResponse.json(bookName,{status:200})
-};
